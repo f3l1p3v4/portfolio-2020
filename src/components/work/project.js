b878a61b97ee2c87b356ac2213f7220a990e4a53
f3l1p3v4 <fvaldez018@gmail.com>
2020-03-26 18:40:57 -0400

    Altera responsividade

--- src/components/work/project.js
+++ src/components/work/project.js
@@ -1,6 +1,6 @@
-import React from "react"
-import Img from "gatsby-image"
-import { Link } from "gatsby"
+import React from "react";
+import Img from "gatsby-image";
+import { Link } from "gatsby";
 
 const Project = ({ project }) => (
   <Link to={`${project.page}`}>
