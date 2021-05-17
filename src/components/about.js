93ab8ff2b5d46cc2a5984bc7b2818943df02d8a8
Felipe Valdez <47610639+f3l1p3v4@users.noreply.github.com>
2021-05-17 09:56:56 -0400

    adding information

--- src/components/about.js
+++ src/components/about.js
@@ -11,16 +11,18 @@ const About = () => (
       <div className="profile">
         <h1>Olá,</h1>
         <p>
-          Tenho 23 anos, sou um dev desde de 2016 quando iniciei Ciência da
-          Computação na faculdade Uniderp em Campo Grande-MS, foi quando conheci
-          um pouco mais sobre programação e logo me apaixonei por códigos,
-          quando completei aproximadamente 2 anos de estudo começei a procurar
-          mais sobre codar, foi quando fiquei estagnado com tantas tecnologias,
-          e não sabia qual era a ideal ou qual melhor me posicionaria no mercado
-          de trabalho, então tranquei a faculdade para iniciar um curso
-          profissionalizante de programação Web na High Tech e que finalizei em
-          2019 e hoje tenho como referência de estudo a Rocktseat e Udemy.{" "}
-          <a href="/#contact">Quer saber mais?</a>
+          Tenho 24 anos, estudo programação desde 2018 e logo me apaixonei por
+          códigos. Atualmente estou trabalhando como freelancer e sou criador da{" "}
+          <a
+            href="https://devload.com.br"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Devload
+          </a>
+          , a idéia é ajudar pessoas que estão querendo começar na área de
+          desenvolvimento com conteúdos nas redes sociais e cursos que sou
+          afiliado. <a href="/#contact">Quer saber mais?</a>
         </p>
       </div>
     </div>
