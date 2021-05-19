f3887f527e5103de96dd68e3052a60a64e6b05b2
Felipe Valdez <47610639+f3l1p3v4@users.noreply.github.com>
2021-05-19 11:03:35 -0400

    update

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,14 @@
 import React from "react"
-import { Link } from 'gatsby'
+import { Link } from "gatsby"
 
 const Error = () => (
   <section className="error-content">
     <h1 className="error-code">error_404</h1>
-    <p className="error-description">Sorry, the page you are looking for <br> could not be found :(</p>
-    <p className="error-link"><Link to="https://felipevaldez.netlify">go to the front page →</Link></p>
+    <p className="error-description">Sorry, the page you are looking for </p>
+    <p className="error-description">could not be found :(</p>
+    <p className="error-link">
+      <Link to="https://felipevaldez.netlify">go to the front page →</Link>
+    </p>
   </section>
 )
 
