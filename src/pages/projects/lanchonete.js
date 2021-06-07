ebf8b99f4bf4acb53f9a538f2434c6522d3076ee
Felipe Valdez <47610639+f3l1p3v4@users.noreply.github.com>
2021-06-07 11:56:19 -0400

    update

--- src/pages/projects/lanchonete.js
+++ src/pages/projects/lanchonete.js
@@ -42,29 +42,29 @@ const Bilhetagem = () => (
             <div className="project-image">
               <Img
                 fluid={
-                  data.allProjectsJson.edges[4].node.image.childImageSharp.fluid
+                  data.allProjectsJson.edges[5].node.image.childImageSharp.fluid
                 }
                 style={{ maxHeight: 450 }}
               />
             </div>
             <div className="projects-nav">
               <div className="projects-nav-link">
-                <Link to="/projects/portfolio">
+                <Link to="/projects/bilhetagem">
                   <IoIosArrowDropleftCircle /> Projeto anterior
                 </Link>
               </div>
               <div className="spacer" />
               <div className="projects-nav-link right">
-                <Link to="/projects/lanchonete">
-                  Próximo Projeto <IoIosArrowDroprightCircle />
+                <Link to="/#projects">
+                  Voltar ao portfólio <IoIosArrowDroprightCircle />
                 </Link>
               </div>
             </div>
-            <h1>Sistema de Bilhetagem</h1>
+            <h1>Cardápio de Lanchonete</h1>
             <div className="info">
               <div className="buttons">
                 <a
-                  href={data.allProjectsJson.edges[4].node.link}
+                  href={data.allProjectsJson.edges[5].node.link}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -72,7 +72,7 @@ const Bilhetagem = () => (
                 </a>
               </div>
             </div>
-            <p>{data.allProjectsJson.edges[4].node.description}</p>
+            <p>{data.allProjectsJson.edges[5].node.description}</p>
 
             <div className="technologies">
               <h3>Construido com:</h3>
--- src/pages/projects/portfolio.js
+++ src/pages/projects/portfolio.js
@@ -2,8 +2,8 @@ import React from "react"
 import { Link, StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import {
-    IoIosArrowDropleftCircle,
-    IoIosArrowDroprightCircle,
+  IoIosArrowDropleftCircle,
+  IoIosArrowDroprightCircle,
 } from "react-icons/io"
 import { DiHtml5, DiSass, DiReact } from "react-icons/di"
 import { FaDesktop, FaCode } from "react-icons/fa"
@@ -12,8 +12,8 @@ import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 
 const Portfolio = () => (
-    <StaticQuery
-        query={graphql`
+  <StaticQuery
+    query={graphql`
       query {
         allProjectsJson {
           edges {
@@ -35,69 +35,69 @@ const Portfolio = () => (
         }
       }
     `}
-        render={data => (
-            <Layout>
-                <SEO title="Project | Portfolio de Desenvolvedor" />
-                <section id="portfolio-projects">
-                    <div className="container">
-                        <div className="project-image">
-                            <Img
-                                fluid={
-                                    data.allProjectsJson.edges[3].node.image.childImageSharp.fluid
-                                }
-                                style={{ maxHeight: 450 }}
-                            />
-                        </div>
-                        <div className="projects-nav">
-                            <div className="projects-nav-link">
-                                <Link to="/projects/bethehero">
-                                    <IoIosArrowDropleftCircle /> Projeto Anterior
+    render={(data) => (
+      <Layout>
+        <SEO title="Project | Portfolio de Desenvolvedor" />
+        <section id="portfolio-projects">
+          <div className="container">
+            <div className="project-image">
+              <Img
+                fluid={
+                  data.allProjectsJson.edges[3].node.image.childImageSharp.fluid
+                }
+                style={{ maxHeight: 450 }}
+              />
+            </div>
+            <div className="projects-nav">
+              <div className="projects-nav-link">
+                <Link to="/projects/bethehero">
+                  <IoIosArrowDropleftCircle /> Projeto Anterior
                 </Link>
-                            </div>
-                            <div className="spacer" />
-                            <div className="projects-nav-link right">
-                                <Link to="/#projects">
-                                    Voltar ao Portfolio <IoIosArrowDroprightCircle />
-                                </Link>
-                            </div>
-                        </div>
-                        <h1>Meu Portfolio de Dev</h1>
-                        <div className="info">
-                            <div className="buttons">
-                                <a href="/">
-                                    <FaDesktop />  Visitar Site
+              </div>
+              <div className="spacer" />
+              <div className="projects-nav-link right">
+                <Link to="/bilhetagem">
+                  Próximo projeto <IoIosArrowDroprightCircle />
+                </Link>
+              </div>
+            </div>
+            <h1>Meu Portfolio de Dev</h1>
+            <div className="info">
+              <div className="buttons">
+                <a href="/">
+                  <FaDesktop /> Visitar Site
                 </a>
-                                <a
-                                    href={data.allProjectsJson.edges[3].node.github}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    <FaCode /> Ver Código
+                <a
+                  href={data.allProjectsJson.edges[3].node.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaCode /> Ver Código
                 </a>
-                            </div>
-                        </div>
-                        <p>{data.allProjectsJson.edges[3].node.description}</p>
+              </div>
+            </div>
+            <p>{data.allProjectsJson.edges[3].node.description}</p>
 
-                        <div className="technologies">
-                            <h3>Construido com:</h3>
+            <div className="technologies">
+              <h3>Construido com:</h3>
 
-                            <div className="icons">
-                                <div className="icon">
-                                    <DiHtml5 />
-                                </div>
-                                <div className="icon">
-                                    <DiSass />
-                                </div>
-                                <div className="icon">
-                                    <DiReact />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </Layout>
-        )}
-    />
+              <div className="icons">
+                <div className="icon">
+                  <DiHtml5 />
+                </div>
+                <div className="icon">
+                  <DiSass />
+                </div>
+                <div className="icon">
+                  <DiReact />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </Layout>
+    )}
+  />
 )
 
 export default Portfolio
