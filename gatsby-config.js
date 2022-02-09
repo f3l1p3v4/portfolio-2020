13e9e808758d4184343578051ec4a585f001a379
Felipe Valdez <47610639+f3l1p3v4@users.noreply.github.com>
2022-02-09 16:37:29 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Felipe Valdez - Developer`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Site de portfolio, para mostrar meus melhores trabalhos e falar um pouco sobre min.`,
+    author: `@Felipe`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
